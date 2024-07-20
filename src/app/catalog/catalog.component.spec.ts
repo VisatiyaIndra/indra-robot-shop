fd91a6019c45c31fe1a70dbace7492981bdeb5a7
Indra Visatiya <visatiyaindra@gmail.com>
2024-07-20 16:14:15 +1200

    Fixed workflow

--- src/app/catalog/catalog.component.spec.ts
+++ src/app/catalog/catalog.component.spec.ts
@@ -20,24 +20,6 @@ describe('CatalogComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should get the correct image URL', () => {
-    const product: IProduct = {
-      id: 1,
-      description: 'A robot head with a camera.',
-      name: 'Camera',
-      imageName: 'head-camera.png',
-      category: 'Heads',
-      price: 150.5,
-      discount: 0,
-    };
-
-    const expectedUrl = 'assets/images/robot-parts/head-camera.png';
-
-    const result = component.getImageUrl(product);
-
-    expect(result).toEqual(expectedUrl);
-  });
-
   it('should get the correct filtered products', () => {
     const expectedFilteredProducts = [
       {
