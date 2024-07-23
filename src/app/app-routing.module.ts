1a40b102cbde21fcc51451928e40221a83116764
Indra Visatiya <visatiyaindra@gmail.com>
2024-07-23 21:49:49 +1200

    Sign in form implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
-import { Router, RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { CartComponent } from './cart/cart.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
 const routes: Routes = [
   { path: "home", component: HomeComponent, title: "Home - Indra's Robot Shop" },
   { path: "catalog", component: CatalogComponent, title: "Catalog - Indra's Robot Shop" },
   { path: "cart", component: CartComponent, title: "Cart - Indra's Robot Shop" },
+  { path: "sign-in", component: SignInComponent, title: "Sign In - Indra's Robot Shop" },
   { path: "", redirectTo: "/home", pathMatch: "full" }
 ];
 
--- src/app/site-header/site-header.component.ts
+++ src/app/site-header/site-header.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { IUser } from '../user/user.model';
+import { UserService } from '../user/user.service';
 
 @Component({
   selector: 'bot-site-header',
   templateUrl: './site-header.component.html',
   styleUrls: ['./site-header.component.css']
 })
-export class SiteHeaderComponent {
+export class SiteHeaderComponent implements OnInit {
+  isAuthenticated: IUser | null = null;
+  showSignOutMenu: boolean = false;
 
-  constructor() { }
+  constructor(private userService: UserService, private router: Router) { }
 
+  ngOnInit() {
+    this.userService.getUser().subscribe({
+      next: (user) => this.isAuthenticated = user
+    });
+  }
+
+  toggleSignOutMenu() {
+    this.showSignOutMenu = !this.showSignOutMenu;
+  }
+
+  signOut() {
+    this.userService.signOut();
+    this.showSignOutMenu = false;
+    this.router.navigate(['sign-in']);
+  }
 }
